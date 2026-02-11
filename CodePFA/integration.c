7d62b765840f2241fe4e90deb3fb5b35c49e79a3
Antoine K <antoine.kremer@epita.fr>
2026-02-11 12:39:03 +0100

    first period

--- CodePFA/integration.c
+++ CodePFA/integration.c
@@ -8,7 +8,27 @@
 
 bool setQuadFormula(QuadFormula* qf, char* name)
 {
-  return true;
+
+	//our code, only updates the name for now
+
+	if (strcmp(name, "left")!=0 && strcmp(name, "right")!=0 && strcmp(name,"middle")!=0 && strcmp(name, "trapezes")!=0 && strcmp(name, "simpson")!=0 && strcmp("gauss2",name)!=0 && strcmp(name,"gauss3")!=0){
+
+		return false;
+
+
+	}
+
+	int current = 0;
+	while (*(name+current)!=0){
+
+		(*qf).name[current] = *(name+current);
+		current++;
+
+	}
+
+	(*qf).name[current] = '\0';
+
+	return true;
 }
 
 /* This function is not required ,but it may useful to debug */
@@ -27,7 +47,29 @@ void printQuadFormula(QuadFormula* qf)
 */
 double integrate(double (*f)(double), double a, double b, int N, QuadFormula* qf)
 {
-  return 0.0;
+	float sum = 0;
+	float bemoinzasurenne = (b-a)/N;
+
+	if (strcmp(qf->name, "left")==0){
+		for (int i=0;i<N;i++){
+			sum+=f(a+i*bemoinzasurenne) *bemoinzasurenne;
+		}
+	}
+	if (strcmp(qf->name, "right")==0){
+		for (int i=1;i<N+1;i++){
+			sum+=f(a+(i)*bemoinzasurenne) * bemoinzasurenne;
+		}
+	}
+	if (strcmp(qf->name, "middle")==0){
+		for (int i=0;i<N;i++){
+			sum+= f( a+  i*bemoinzasurenne + (bemoinzasurenne   )/2 ) *bemoinzasurenne;
+		}
+	}
+
+	if (strcmp(qf->name, ""))
+	
+	return sum;
+	return 0.0;
 }
 
 double integrate_dx(double (*f)(double), double a, double b, double dx, QuadFormula* qf)
--- CodePFA/test_integration.c
+++ CodePFA/test_integration.c
@@ -5,9 +5,33 @@
 
 #include "integration.h"
 
+#include <math.h>
+
+
+
+double f(double t)
+{
+return t*t;
+}
+
 /* Code here everything you need to test the integration methods and show your numericzal results */
 
 int main()
 {
-  return 0;
+	QuadFormula qf;
+	double I1, I2, I3;
+	setQuadFormula(&qf, "left");
+	I1 = integrate(f, 0, 1, 100, &qf);
+	setQuadFormula(&qf, "right");
+	I2 = integrate(f, 0, 1, 100, &qf);
+	setQuadFormula(&qf, "middle");
+	I3 = integrate(f, 0, 1, 100, &qf);
+	
+	printf("%f, %f, %f", I1, I2, I3);
+	return 0;
 }
+
+
+
+
+
